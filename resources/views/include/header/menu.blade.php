78daa348fb5dd19d3595c2a9fae47d62d617d60c
Serhii <ex@example.com>
2023-03-23 13:22:03 +0200

    add menu from bd and correct front menu

--- resources/views/include/header/menu.blade.php
+++ resources/views/include/header/menu.blade.php
@@ -2,31 +2,29 @@
   <div class="link_left">
     <nav class="header__menu menu">
       <ul class="menu__list">
-        <li class="menu__item"><a href="{{$menu->leftMenu->link}}" class="menu__link">{{$menu->leftMenu->title}}</a></li>
-        <li class="menu__item"><a href="#" class="menu__link">Blog</a></li>
-        <li class="menu__item"><a href="#" class="menu__link">For Restraunts</a></li>
+          @foreach($leftMenu as $item)         
+        <li class="menu__item"><a href="{{$item->link}}" class="menu__link">{{$item->title}}</a></li>
+        @endforeach       
       </ul>
       @include('include.header.iconMenu')
-
-
     </nav>
     <div class="menus "><a class="" href="#">Menu</a></div>
-
-
     <div class="link_restrance ">
       <a class="" href="#">For restaraunts</a>
     </div>
   </div>
-
-
   <div class=" block_icon "><img class=" link_icon " src="{{asset('images/icon/Frame.png')}}"  alt=""></div>
-
-
   <ul class="link_right">
-    <li class="link__right"><a class="" href="#">Login</a></li>
-    <li class="link__right"><a class="" href="#">Sign up</a></li>
-    <li class="link__right"   ><button data-type="popup-1"  class="" href="#"><img src="{{asset('images/icon/search-ico.png')}}" 
-          alt=""></button></li>
+    @foreach($rightMenu as $index => $item)
+    @if ($index != 2)
+    <li class="link__right"><a class="" href="{{$item->link}}">{{$item->title}}</a></li>
+    @else
+    
+    <li class="link__right" ><button data-type="popup-1"  class="" href="{{$item->link}}"><img src="{{asset($item->title)}}" alt=""></button></li>
+    @endif
+    
+    @endforeach
+  
   </ul>
 
   
